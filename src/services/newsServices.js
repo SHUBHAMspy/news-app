429098e1a0c5fd346740744cd8a47e440472a8a4
Shubham Pandey <shubhampandey1964@gmail.com>
2022-07-28 14:39:22 +0530

    Added services

---
+++ src/services/newsServices.js
@@ -0,0 +1,46 @@
+//API URL
+export const API_URL = 'https://newsapi.org/v2';
+import axios from 'axios';
+
+export const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+//export const API_PARAMS = `&pageSize=${PAGESIZE}`;
+
+//API End Points
+export const HEADLINES = `${API_URL}/top-headlines?apiKey=${API_KEY}`;
+export const SOURCES = `${API_URL}/top-headlines/sources?apiKey=${API_KEY}`;
+
+export async function getHeadlines(country = "in"){
+  try {
+    let url = `${HEADLINES}&country=${country}`
+    const response = await axios.get(url);
+    const data = await response.data
+    return data
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+export async function getSources(country = "in"){
+  try {
+    let url = `${SOURCES}&country=${country}`
+    const response = await axios.get(url);
+    const data = await response.data
+    return data
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function getHeadlinesBySource(source){
+  
+  try {
+    let url = `${HEADLINES}&sources=${source.split(' ').join('-').toLowerCase()}`
+    console.log(url);
+    const response = await axios.get(url);
+    console.log(response);
+    const data = await response.data
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
